c088c632b077d9695f67b59a4844a0444a8a2475
yoonsejong <oxwk123@gmail.com>
2022-06-17 09:25:53 +0900

    0617 js study

--- Basic0616.js
+++ Basic0616.js
@@ -1,9 +1,9 @@
 console.log("벨로퍼트 블로그 callback개념");
 function work(callback) {
   setTimeout(() => {
-    const start = Date.now;
+    const start = Date.now();
     for (let i = 0; i < 10000000; i++) {}
-    const end = Date.now;
+    const end = Date.now();
     console.log(end - start + "ms");
     callback();
   }, 0);
@@ -17,12 +17,92 @@ console.log("작업종료");
 
 console.log("Promise 시작");
 
-const myPromise = new Promise((reselve, reject) => {
+const myPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    reselve(1);
+    resolve(1);
+    reject(new Error());
   }, 1000);
 });
 
-myPromise.then((n) => {
-  console.log(n);
-});
+myPromise
+  .then((n) => {
+    console.log(n);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+function increaseAndPrint(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const value = n + 1;
+      if (value === 5) {
+        const error = new Error();
+        error.name = "ValueIsFiveError";
+        reject(error);
+        return;
+      }
+      console.log(value);
+      resolve(value);
+    }, 1000);
+  });
+}
+
+increaseAndPrint(0)
+  .then((n) => {
+    return increaseAndPrint(n);
+  })
+  .then((n) => {
+    return increaseAndPrint(n);
+  })
+  .then((n) => {
+    return increaseAndPrint(n);
+  })
+  .then((n) => {
+    return increaseAndPrint(n);
+  })
+  .then((n) => {
+    return increaseAndPrint(n);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+/* 위 코드를 아래와 같이 정리 할 수 있다. */
+function increaseAndPrint(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const value = n + 1;
+      if (value === 5) {
+        const error = new Error();
+        error.name = "ValueIsFiveError";
+        reject(error);
+        return;
+      }
+      console.log(value);
+      resolve(value);
+    }, 1000);
+  });
+}
+
+increaseAndPrint(0)
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .then(increaseAndPrint)
+  .catch((e) => {
+    console.error(e);
+  });
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function process() {
+  console.log("안녕하세요");
+  await sleep(1000);
+  console.log("반갑습니다");
+}
+
+process();
