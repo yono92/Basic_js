7cd805e84354a239db68d7d6feed8e477fc9491a
yoonsejong <oxwk123@gmail.com>
2022-06-23 17:51:26 +0900

    fetch study

---
+++ fetchprac.js
@@ -0,0 +1,7 @@
+fetch("http://localhost:3000")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+/**
+ * https://developer.mozilla.org/ko/docs/Web/API/Fetch_API/Using_Fetch
+ */
