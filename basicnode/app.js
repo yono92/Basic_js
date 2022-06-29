43edac35176dbe703d6f5aa3debb0f1728e98017
yoonsejong <oxwk123@gmail.com>
2022-06-29 17:45:56 +0900

    multipart/form data post

--- basicnode/app.js
+++ basicnode/app.js
@@ -27,26 +27,62 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  console.log("모든 요청에 다 실행됩니다.");
-  next();
+const multer = require("multer");
+const fs = require("fs");
+
+try {
+  fs.readdirSync("uploads");
+} catch (error) {
+  console.error("upload 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limit: { fileSize: 5 * 1024 * 1024 },
 });
 
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("GET / 요청에서만 실행됩니다.");
-    next();
-  },
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
+});
+
+app.post(
+  "/upload",
+  upload.fields([{ name: "image1" }, { name: "image2" }]),
   (req, res) => {
-    throw new Error("에러는 에러처리 미들웨어로 간다.");
+    console.log(req.files, req.body);
+    res.send("ok");
   }
 );
 
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send(err.message);
-});
+// app.use((req, res, next) => {
+//   console.log("모든 요청에 다 실행됩니다.");
+//   next();
+// });
+//
+// app.get(
+//   "/",
+//   (req, res, next) => {
+//     console.log("GET / 요청에서만 실행됩니다.");
+//     next();
+//   },
+//   (req, res) => {
+//     throw new Error("에러는 에러처리 미들웨어로 간다.");
+//   }
+// );
+
+// app.use((err, req, res, next) => {
+//   console.log(err);
+//   res.status(500).send(err.message);
+// });
 
 // app.get("/", (req, res) => {
 //   // res.send("Hello World");
