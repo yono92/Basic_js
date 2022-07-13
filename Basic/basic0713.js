dda414b7629ff24078e403ad257d713c7d27e223
yoonsejong <oxwk123@gmail.com>
2022-07-13 17:56:45 +0900

    스프레드 연산자 공부

---
+++ Basic/basic0713.js
@@ -0,0 +1,21 @@
+const cart = ["Naming and Necssity", "Alice in Wonderland"];
+
+console.log(...cart);
+
+const copycart = [...cart];
+
+console.log(copycart);
+
+function removeItem(items, removable) {
+  const updated = [];
+  for (let i = 0; i < items.length; i++) {
+    updated.push(items[i]);
+  }
+  return updated;
+}
+
+function removeItem(items, removable) {
+  const index = items.indexOf(removable);
+  items.splice(index, 1);
+  return items;
+}
